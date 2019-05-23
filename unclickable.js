db03ef7ba473c16fe6cb8386259bead13e6acc2d
Glitch (jolson-examples) <none>
2019-05-23 14:08:38 +0000

    🌃🏜 Checkpoint ./index.html:574715/121 ./unclickable.js:574715/133

--- unclickable.js
+++ unclickable.js
@@ -1,7 +1,11 @@
 function setup() {
   createCanvas(windowWidth, windowHeight)
+  d = 20
+  x = random(d/2, windowWidth - d/2)
+  y = random(d/2, windowHeight - d/2)
 }
 
 function draw() {
   background(220)
+  ellipse(x, y )
 }
