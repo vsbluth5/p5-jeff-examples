91caa4d398224c994822287742372c8bab33673a
Glitch (jolson-examples) <none>
2019-05-23 14:29:35 +0000

    🐸🗼 Checkpoint ./unclickable.js:574715/528 ./hardgame.html:574715/4

--- unclickable.js
+++ unclickable.js
@@ -1,11 +1,18 @@
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  d = 20
+  d = 50
   x = random(d/2, windowWidth - d/2)
   y = random(d/2, windowHeight - d/2)
 }
 
 function draw() {
+  if (dist(mouseX, mouseY, x, y) <= d + 20) {
+    x = random(d/2, windowWidth - d/2)
+    y = random(d/2, windowHeight - d/2)
+  }
   background(220)
-  ellipse(x, y )
+  fill(240, 220, 220)
+  ellipse(x, y, d) 
+  fill(0)
+  text("click me", x-21, y)
 }
